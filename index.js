f5f54bc0e419bd6f79aa46894ecd54d086c63cc1
Robert Lin <Rlin415@gmail.com>
2015-09-30 06:56:49 -0700

    Modified html and added click function on usernames to show timeline

--- index.js
+++ index.js
@@ -1,16 +1,36 @@
 $(document).ready(function(){
-  var $body = $('body');
+
+  var $header = $('h1');
+  var $allTweets = $('.allTweets');
+  var $selectedUsr = $('.selectedUsr');
+
 
   var index = streams.home.length - 1;
   while(index >= 0){
     var tweet = streams.home[index];
-    var $tweet = $("<h2 class='tweets'><span class='userName'>" +
-      tweet.user + "</span>" + tweet.message + "</h2>");
+    var $tweet = $("<h2 class='tweets'><span class='userName'>@" +
+      tweet.user + "</span>: " + tweet.message + "</h2>");
     var $timeStamp = $("<h3 class='timeStamp'></h3>")
     $timeStamp.text(tweet.created_at);
-    $timeStamp.appendTo($('.allTweets'));
-    $tweet.appendTo($('.allTweets'));
+    $timeStamp.appendTo($allTweets);
+    $tweet.appendTo($allTweets);
     index -= 1;
+  }
+
+  var $userName = $('.userName');
+  var currUser;
+  $userName.click(function(event) {
+    $header.attr('hidden', true);
+    $selectedUsr.attr('hidden', false);
+    currUser = event.target.innerText;
+    currUser = currUser.slice(1).toString();
+    $selectedUsr.text(currUser);
+    $allTweets.attr('hidden', true);
+    showUserTimeline();
+  });
+
+  function showUserTimeline() {
+    
   }
 
 });
